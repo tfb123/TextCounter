07ee18b037d8adb07781c3ef7ef2ad000b447a34
tfb123 <mil.xomiak@gmail.com>
2022-10-16 20:21:11 +0300

    TextCount implementation

--- app/src/main/java/com/example/textcounter/MainActivity.java
+++ app/src/main/java/com/example/textcounter/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.textcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import utils.TextCounter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spSelectionOptions;
+    private EditText edPhrase;
+    private TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.edPhrase = findViewById(R.id.edPhrase);
+        this.tvMain = findViewById(R.id.tvMain);
+
+        this.spSelectionOptions = (Spinner) findViewById(R.id.spSelectionOptions);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.selection_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.spSelectionOptions.setAdapter(adapter);
+
+
+    }
+
+    public void onBtnCountClick(View view) {
+        if(this.spSelectionOptions.getSelectedItem().toString().equals(getResources().getString(R.string.chars_selection))){
+            this.tvMain.setText(TextCounter.getCharsCount(this.edPhrase.getText().toString()));
+        }
     }
 }
---
+++ app/src/main/java/utils/TextCounter.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class TextCounter {
+    public static String getCharsCount(String input) {
+        return String.valueOf(input.length());
+    }
+}
